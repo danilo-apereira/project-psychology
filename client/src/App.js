47f8b40f30af761ce422ad6c68d7e9e34cec8a74
Danilo Augusto <contato.daniloapereira@gmail.com>
2023-10-15 15:15:46 -0300

    Adicionado navegação de páginas no cliente

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './Pages/HomePage/HomePage';
+
+import Signin from './Pages/AuthPage/Signin';
+import Signup from './Pages/AuthPage/Signup';
+
+import NotFound from './Pages/NotFound/NotFound';
+
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+
+        <Route exact path="/auth/signin" element={<Signin />} />
+        <Route exact path="/auth/signup" element={<Signup />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
